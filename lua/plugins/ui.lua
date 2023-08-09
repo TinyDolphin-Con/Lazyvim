6b1ac7dfe1fbf73d580d14c215fc8b617a6f3a1d
TinyDolphin-Con <tiny_dolphin@163.com>
2023-08-09 11:45:01 +0800

    feat(Lazyvim): First commit

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -32,7 +32,7 @@ return {
           },
         },
         -- enable this to exit Neovim when only edgy windows are left
-        exit_when_last = true,
+        exit_when_last = false,
         -- close edgy when all windows are hidden instead of opening one of them
         -- disable to always keep at least one edgy split visible in each open section
         close_when_all_hidden = true,
